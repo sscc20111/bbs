c1b255b3da99af234e9cf70678dbdc7c6db0359e
sscc20111 <mw92559404@gmail.com>
2023-10-29 22:16:48 +0900

    기초로직 완성(로컬스토리지_ 이후작업-db연결)

--- src/App.js
+++ src/App.js
@@ -1,60 +1,13 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import gsap from 'gsap';
 import { Draggable } from "gsap/Draggable";
 import { Flip } from 'gsap/Flip';
+import { Button, Container, FloatingLabel, FormControl, Stack } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
 gsap.registerPlugin(Draggable, Flip) 
-
-const Note = ({ index, children, style, onChange, onRemove, testset }) => {
-    const [editing, setEditing] = useState(false);
-
-    useEffect(()=>{
-      Draggable.create(".note",{
-        onDragEnd: function(e) {
-          const target = e.target.closest('.note');
-          const index = target.getAttribute('data-index');
-
-          testset(target.style.transform, index)
-        }
-      });
-    },[setEditing])
-
-    const edit = (e) => {
-        setEditing(true);
-        Draggable.get(e.target.closest('.note')).disable();
-      };
-      
-      const save = (e) => {
-        onChange(newTextRef.current.value, index);
-        setEditing(false);
-        Draggable.get(e.target.closest('.note')).enable();
-    };
-
-    const remove = () => {
-        onRemove(index);
-    };
-
-    const newTextRef = useRef();
-
-    if (editing) {
-        return (
-            <div className="note" style={style} data-index={index}>
-                <textarea ref={newTextRef} defaultValue={children} className="form-control"></textarea>
-                <button onClick={save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
-            </div>
-        );
-    } else {
-        return (
-            <div className="note" style={style} data-index={index}>
-                <p>{children}</p>
-                <p>{index}</p>
-                <span>
-                    <button onClick={edit} className="btn btn-primary glyphicon glyphicon-pencil" />
-                    <button onClick={remove} className="btn btn-danger glyphicon glyphicon-trash" />
-                </span>
-            </div>
-        );
-    }
-};
 
 const style = () => {
   const randomBetween = (min, max) => {
@@ -69,41 +22,125 @@ const style = () => {
   return style
 }
 
+const flip = (from, to) => {
+  const flipFrom = document.querySelector(from)
+  const flipTo = document.querySelector(to)
+  const state = Flip.getState(from + ', ' + to);
+  flipFrom.classList.toggle("flipActive");
+  flipTo.classList.toggle("flipActive");
+  Flip.from(state, {
+    duration: 0.6,
+    fade: true,
+    scale: true,
+    absolute: true,
+    toggleClass: "flipping",
+    ease: "power3.inOut"
+  });
+}
+
+const Note = ({ index, children, style, onRemove, drag, modify, state }) => {
+    useEffect(()=>{
+      Draggable.create(".note",{
+        onDragEnd: function(e) {
+          const target = e.target.closest('.note');
+          const index = target.getAttribute('data-index');
+
+          drag(target.style.transform, index)
+        }
+      });
+    },[drag])
+
+    const remove = () => {
+        onRemove(index);
+    };
+
+    const modifyTarget = (e) => {
+      const target = e.target.closest('.note')
+      const modifyTarget = `.${target.classList[1]}`
+      modify(modifyTarget, '.flipForm', index)
+    }
+
+    if(state){
+      return (
+          <div className={'note flipIndex' + index} style={style} data-index={index} data-flip-id="flipform">
+              <p>{children}</p>
+              <p>{index}</p>
+              <p>{state.toString()}</p>
+              <span>
+                  <button onClick={modifyTarget} className="btn btn-primary glyphicon glyphicon-pencil" />
+                  <button onClick={remove} className="btn btn-danger glyphicon glyphicon-trash" />
+              </span>
+          </div>
+      );
+    }else{
+      return (
+          <div className={'note flipIndex' + index + ' dumyNote'} style={style} data-index={index} data-flip-id="flipform">
+              <p>{children}</p>
+              <p>{index}</p>
+              <p>{state.toString()}</p>
+              <span>
+                  <button onClick={modifyTarget} className="btn btn-primary glyphicon glyphicon-pencil" />
+                  <button onClick={remove} className="btn btn-danger glyphicon glyphicon-trash" />
+              </span>
+          </div>
+      );
+    }
+};
+const NoteFlip = ({textsave, flipOut,modifyText}) => {
+  const [noteText, setNoteText] = useState('');
+  // useEffect(()=>{
+  //   setNoteText()
+  // },[])
+  const save = () => {
+    textsave(noteText)
+    setNoteText('')
+  }
+  const cancel = () => {
+    flipOut()
+  }
+
+  return (
+    <Container mx='auto' className='flipForm mw-70 p-4 rounded-4 position-fixed bg-warning-subtle' data-flip-id="flipform" style={{ maxWidth:'720px', zIndex:'3000'}}>
+      <Stack direction="horizontal">
+        <div className='username px-3 mb-3'>작성자</div>
+        <div className='date ms-auto px-3 mb-3'>날짜</div>
+      </Stack>
+      <FloatingLabel className='mb-4' controlId="noteText" label='방명록을 작성해주세요'>
+        <FormControl as="textarea" placeholder="Leave a comment here" style={{ height: '300px' }} value={noteText} onChange={(e) => setNoteText(e.target.value)}></FormControl>
+      </FloatingLabel>
+      <Stack direction='horizontal'>
+        <Button className='me-2 ms-auto' onClick={save}>save</Button>
+        <Button onClick={cancel}>cancel</Button>
+      </Stack>
+    </Container>
+  )
+}
 const Board = () => {
     const savenotes = JSON.parse(localStorage.getItem('bbs_data_style'));
-    const [notes, setNotes] = useState(savenotes || []);
+    const [notes, setNotes] = useState(savenotes || [{id:0, note:'', style:style()}]);
     const [id, setId] = useState(0);
+    const [FlipArry, setFlip] = useState([]);
+    
     useEffect(() => {
       if (savenotes && savenotes.length !== 0) {
         setId(savenotes.slice(-1)[0].id + 1);
       } else {
         setId(0);
       }
-      console.log(id)
-    }, []);
-    const add = (text) => {
-        setNotes([...notes, { id: id+1, note: text, style: style()}]);
-        setId(id+1)
-    };
+    }, [savenotes]);
 
-    const update = (newText, i) => {
-      const updatedNotes = notes.map(note => {
-        if (note.id === parseInt(i)) {
-          return { ...note, note:newText };
-        }
-        return note;
-      });
-      setNotes(updatedNotes);
-    };
+    useEffect(()=>{
+      localStorage.setItem('bbs_data_style', JSON.stringify(notes));
+    },[notes])
     
     const remove = (i) => {
       const updatedNotes = notes.filter(note => note.id !== i);
       setNotes(updatedNotes);
     };
     
-    const test = (transform, i) => {
-      const test = JSON.parse(localStorage.getItem('bbs_data_style'));
-      const updatedNotes = test.map(note => {
+    const dragset = (transform, i) => {
+      const Localnotes = JSON.parse(localStorage.getItem('bbs_data_style'));
+      const updatedNotes = Localnotes.map(note => {
         if (note.id === parseInt(i)) {
           return { ...note, style:{...note.style, transform:transform} };
         }
@@ -111,58 +148,57 @@ const Board = () => {
       });
       setNotes(updatedNotes);
     }
-    
-    useEffect(()=>{
-      localStorage.setItem('bbs_data_style', JSON.stringify(notes));
-    },[add, remove, test])
-    
-    // const fliptest = () => {
-    //   const flipbtn = document.querySelector('.flipTestBtn')
-    //   const flipContent = document.querySelector('.flipTestContent')
-    //   const state = Flip.getState(".flipTestBtn, .flipTestContent");
-    //   flipbtn.classList.toggle("active");
-    //   flipContent.classList.toggle("active");
-    //   Flip.from(state, {
-    //     duration: 2,
-    //     fade: true,
-    //     absolute: true,
-    //     toggleClass: "flipping",
-    //     ease: "power1.inOut"
-    //   });
-    //   console.log('flip')
-    // }
-    // const fliptest2 = () => {
-    //   const flipbtn = document.querySelector('.flipTestBtn2')
-    //   const flipContent = document.querySelector('.flipTestContent')
-    //   const state = Flip.getState(".flipTestBtn2, .flipTestContent");
-    //   flipbtn.classList.toggle("active");
-    //   flipContent.classList.toggle("active");
-    //   Flip.from(state, {
-    //     duration: 2,
-    //     fade: true,
-    //     absolute: true,
-    //     toggleClass: "flipping",
-    //     ease: "power1.inOut"
-    //   });
-    //   console.log('flip')
-    // }
 
-    const createText = () => {
-      
+    const flipOut = () => {
+      flip(FlipArry[0], FlipArry[1])
+      if(FlipArry[0] === '.createBtn'){
+        remove(id-1)
+        setId(id-1)
+      }
     }
+
+
+    const update = (newText, i) => {
+      const updatedNotes = notes.map(note => {
+        if (note.id === i) {
+          return { ...note, note:newText, state:true};
+        }
+        return note;
+      });
+      setNotes(updatedNotes);
+    };
+
+    const noteSave = (noteText) => {
+      if(FlipArry[0] === '.createBtn'){
+        flip('.flipIndex'+(id-1), FlipArry[1])
+        update(noteText, id-1)
+      }else{
+        flip(FlipArry[0], FlipArry[1])
+        update(noteText, FlipArry[2])
+      }
+    }
+
+    const creatNote = () => {
+      setId(id+1)
+      setNotes([...notes, { id: id, style: style(), state:false}]);
+      flip('.createBtn','.flipForm')
+      setFlip(['.createBtn','.flipForm'])
+    }
+
+    const modify = (from, to, index) => {
+      flip(from, to)
+      setFlip([from, to, index])
+    }
+
     return (
         <div className="board">
-            {notes.map((note, index) => (
-                <Note key={note.id} index={note.id} style={note.style} testset={test} onChange={update} onRemove={remove}>
+            {notes.map((note) => (
+                <Note key={note.id} index={note.id} length={id} style={note.style} state={note.state} drag={dragset} modify={modify} onRemove={remove}>
                     {note.note}
                 </Note>
             ))}
-            <button className="btn btn-sm btn-success glyphicon glyphicon-plus" onClick={() => add('New Note')} />
-            
-            <img className='flipTestContent' onClick={createText} data-flip-id="afawfawf"  src="https://placehold.co/600x600"></img>
-            {/* <img className='flipTestBtn' onClick={fliptest} data-flip-id="afawfawf" src="https://placehold.co/200x200"></img>
-            <img className='flipTestContent' onClick={fliptest2} data-flip-id="afawfawf"  src="https://placehold.co/600x600"></img>
-            <img className='flipTestBtn2' data-flip-id="afawfawf" src="https://placehold.co/200x200"></img> */}
+            <button className="createBtn btn btn-sm btn-success glyphicon glyphicon-plus" data-flip-id="flipform" onClick={creatNote} />
+            <NoteFlip textsave={noteSave} flipOut={flipOut}></NoteFlip>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Board from './App';
 import reportWebVitals from './reportWebVitals';
 
